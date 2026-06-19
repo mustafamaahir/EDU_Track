ad0ee7cefdc2228bee098d1bc11971cf658835e0
unknown <mustafamaahir@gmail.com>
2026-06-19 13:36:03 +0100

    .

--- backend/app/main.py
+++ backend/app/main.py
@@ -15,7 +15,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://holicoaching.onrender.com",   # replace with your Vercel URL
+        "https://holicoaching.vercel.app/",   
     ],
     allow_credentials=True,
     allow_methods=["*"],
